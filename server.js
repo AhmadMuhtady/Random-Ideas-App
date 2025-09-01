7979ccc477e2bacd8ca9075ddcb6e5b36bb49c9e
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-01 18:34:42 +0300

    Mongoose Connect & Dotenv

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const port = 5000;
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db.js');
 
+connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
